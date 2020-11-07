5c460c7a2f873d0cdd01037024c32cb95b57e5ee
Edward Poon <edwardpoon1130@gmail.com>
2020-11-07 17:43:06 -0500

    Add the map to the app

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## Setup
+# Setup
+
+## Make sure to restrict the API Key to the release and debug certs: https://developers.google.com/maps/documentation/android-sdk/get-api-key
 You will need to create a file called secure.properties that contains the following:
     MAPS_API_KEY=<API Key goes here> 
     To get this API key, follow these instructions: https://developers.google.com/maps/documentation/android-sdk/get-api-key 
--- app/src/main/java/com/edward/mukbang/MainActivity.kt
+++ app/src/main/java/com/edward/mukbang/MainActivity.kt
@@ -2,10 +2,34 @@ package com.edward.mukbang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapFragment = SupportMapFragment.newInstance(mapOptions())
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, mapFragment).commit()
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(map: GoogleMap?) {
+        this.map = map as GoogleMap
+
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun mapOptions(): GoogleMapOptions {
+        var mapOptions = GoogleMapOptions()
+        // Allows us to also see indoor maps
+        mapOptions.mapType(GoogleMap.MAP_TYPE_NORMAL)
+        return mapOptions
+    }
+
 }
